c872e34cd7eec763cac52058dd65f0fa6f5043b3
ArsenTemirbekov <Arsen.t@flabio.ru>
2017-11-26 15:21:01 +0600

    2 page end

---
+++ app/scripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  // form
+  $('input').click(function(){
+    $('input').parent().removeClass("active");
+    $(this).parent().addClass('active');
+  });
+  // form end
+
+});
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,11 @@
-"use strict";
+'use strict';
+
+$(document).ready(function () {
+
+  // form
+  $('input').click(function () {
+    $('input').parent().removeClass("active");
+    $(this).parent().addClass('active');
+  });
+  // form end
+});
